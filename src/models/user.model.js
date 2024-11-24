82762277912c1a4a1bdd4d1f5b9313e3fd6cc300
ahmetkasap <aahmetkasap@gmail.com>
2024-11-24 13:17:58 +0300

    user model created

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/db.connection';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty : {
+        msg : "name cannot be empty"
+      },
+      len: {
+        args: [3, 50],
+        msg: 'Name must be between 3 and 50 characters',
+      },
+      trim : true
+    },
+  },
+
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+})
+
+export default User
+
